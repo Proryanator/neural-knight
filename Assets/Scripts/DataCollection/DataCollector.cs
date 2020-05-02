652d0e50ac5816f03cbe49ac15557fb308537b18
Ryan Laseter <proryanator@gmail.com>
2020-05-01 22:24:45 -0500

    Started the 'data collection' aspects of the game

---
+++ Assets/Scripts/DataCollection/DataCollector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+/**
+ * Collects a data point object, and passes it to the respective location to store the points/etc.
+ */
+public class DataCollector : MonoBehaviour{
+	
+	public void OnCollisionEnter2D(Collision2D other){
+		CollectDataPoint(other);
+	}
+
+	private void CollectDataPoint(Collision2D other){
+		DataPointType dataPointType = other.gameObject.GetComponent<DataPointType>();
+
+		// only interacting with data points here
+		if (dataPointType == null){
+			return;
+		}
+
+		// depending on the data type, we'll do something different
+		switch (dataPointType){
+			case DataPointType.GoodData:
+				// TODO: implement what happens with good data
+				break;
+			case DataPointType.BadData:
+				// TODO: implement what happens with bad data
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
+		}
+	}
+}
---
+++ Assets/Scripts/DataCollection/DataPoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/**
+ * Holds information about this data point.
+ *
+ * Will hold things like how many points this is worth, what type of data point it is, etc.
+ */
+public class DataPoint : MonoBehaviour{
+	/// <summary>
+	/// The type of data point this object is. Helps tell the player objects what happens when they're collided with.
+	/// </summary>
+	[SerializeField] private DataPointType _dataPointType;
+	
+	
+}
