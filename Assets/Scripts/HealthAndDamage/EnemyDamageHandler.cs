8592ead834b9a358709d9ab0f709e7c28d8aec61
Ryan Laseter <proryanator@gmail.com>
2020-05-01 22:25:20 -0500

    Re-factored the damage system!

---
+++ Assets/Scripts/HealthAndDamage/EnemyDamageHandler.cs
@@ -0,0 +1,18 @@
+﻿using Systems;
+using UnityEngine;
+
+public class EnemyDamageHandler : MonoBehaviour{
+
+	public void OnCollisionEnter2D(Collision2D other){
+		// gather it's health object and damage it if we're supposed to
+		BaseHealth health = other.gameObject.GetComponent<BaseHealth>();
+		
+		if (health != null && CauseDamage(other)){
+			health.Damage();
+		}
+	}
+	
+	private bool CauseDamage(Collision2D other){
+		return other.gameObject.tag.Equals(AllTags.PLAYER);
+	}
+}
