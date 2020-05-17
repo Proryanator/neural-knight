f7697b16c2db9e8ddc7eb1f3de8a4afd3943558a
Ryan Laseter <proryanator@gmail.com>
2020-05-17 12:48:02 -0500

    Getting ready to create the pattern version of the AI path!

--- Assets/Scripts/AIMovementPatterns/FollowPlayerPattern.cs
+++ Assets/Scripts/AIMovementPatterns/FollowPlayerPattern.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using Pathfinding;
+using UnityEngine;
 
 /// <summary>
-/// Forces this data object to follow the player.
+/// Forces this data object to follow the player using A* pathfinding.
 /// </summary>
+[RequireComponent(typeof(Seeker))]
 public class FollowPlayerPattern : AbstractAIMovementPattern{
 	public override void Move(){
-		throw new System.NotImplementedException();
+		
 	}
 }
