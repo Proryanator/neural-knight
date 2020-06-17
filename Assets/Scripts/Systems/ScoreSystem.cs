d2ed71ca169164e169533b9f0167fca4bf801f6d
Ryan Laseter <proryanator@gmail.com>
2020-06-16 22:36:53 -0500

    Turning score system into a singleton instance

--- Assets/Scripts/Systems/ScoreSystem.cs
+++ Assets/Scripts/Systems/ScoreSystem.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 /// <summary>
 /// Intended to hold scores of single (and eventually mutli-player) players.
 ///
-/// Currently only holds single player's score.
+/// Currently only holds single player's score, and acts as a singleton.
+/// Don't access me everywhere!
 /// </summary>
 public class ScoreSystem : MonoBehaviour{
 	
 	[Tooltip("The players current score; incremented by killing enemies and collecting good data.")]
-	[SerializeField] private int _score;
+	[SerializeField] private int _score = 0;
+
+	private static ScoreSystem _instance = null;
 	
 	private void Awake(){
-		_score = 0;
+		if (_instance == null){
+			_instance = this;
+		}else if (_instance != this){
+			Destroy(gameObject);
+		}
 	}
 
 	/// <summary>
 	/// Subscribe to this if you want to know when the score changes!
 	/// </summary>
 	public Action<int> OnScoreChange;
+
+	public static ScoreSystem GetInstance(){
+		return _instance;
+	}
 	
 	/// <summary>
 	/// Adds the given points to the current score.
