68c4976d850387e0288eb67043590a12acdbe24d
Ryan Laseter <proryanator@gmail.com>
2020-07-10 20:18:39 -0500

    New scene, and adding script to prevent legs from inheriting rotation from the parent (legs will determine the direction to face on their own during player's movement).

---
+++ Assets/Scripts/Animations/LegsRotation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls leg movement and behaviors.
+/// </summary>
+public class LegsRotation : MonoBehaviour{
+
+	private Quaternion _initialRotation;
+
+	private void Awake(){
+		_initialRotation = transform.rotation;
+	}
+
+	private void Update(){
+		// stop the rotation being applied from any parent objects
+		transform.rotation = _initialRotation;
+	}
+}
