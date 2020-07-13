17aeef839126e264dcccb936cce2f6e94d5d04ba
Ryan Laseter <proryanator@gmail.com>
2020-07-12 22:27:59 -0500

    Preliminary backwards movement! :D need to fix in-between threshold however.

--- Assets/Scripts/Animations/Algorithm/LegAnimationController.cs
+++ Assets/Scripts/Animations/Algorithm/LegAnimationController.cs
@@ -18,21 +18,28 @@ public class LegAnimationController : MonoBehaviour{
 	private Animator _animator;
 
 	// also needing a reference to the controller! Will be in the parent
-	private Base2DController _controller;
+	private TDC_FaceMouse _faceMouseController;
 
+	// this is the angle threshold that you're allowed to be in for your legs to be moving
+	// in the same direction; otherwise, you'll be moving backwards (with the angle
+	// to face calculated here too
+	[Tooltip("The maximum angle difference between your movement direction and facing direction.")]
+	[SerializeField] private float _turnAngleThreshold = 30;
+	
 	private void Awake(){
 		_animator = GetComponent<Animator>();
-		_controller = GetComponentInParent<Base2DController>();
+		_faceMouseController = GetComponentInParent<TDC_FaceMouse>();
 	}
 
 	// NOTE: using fixed update, to make sure it's called in-line with the physics based movement system
 	private void FixedUpdate(){
-		Vector2 moveDirection = _controller.GetDirection();
+		Vector2 moveDirection = _faceMouseController.GetDirection();
 		// determine the movement direction, and face this object in that direction
 		RotateLegsTowardMovementDirection(moveDirection);
 		
 		// set the movement speed of the animator, if any
 		// if any input is received (as in, if it's not a zero vector), this will be set
+		// TODO: might want to make this the actual amount you're moving, to allow for speed to vary the animation speed
 		if (!moveDirection.Equals(Vector2.zero)){
 			_animator.SetFloat("Speed", 1f);
 		}
@@ -44,27 +51,56 @@ public class LegAnimationController : MonoBehaviour{
 	/// <summary>
 	/// Rotates the legs towards the movement direction.
 	/// </summary>
-	private void RotateLegsTowardMovementDirection(Vector2 directionToFace) {
-		// don't do this if there's no movement
-		if (directionToFace.Equals(Vector2.zero)){
-			return;
+	private void RotateLegsTowardMovementDirection(Vector2 moveDirection) {
+		transform.rotation = ChooseLegDirection(moveDirection);
+	}
+	
+	/// <summary>
+	/// If there's no movement, point your legs in the facing direction.
+	///
+	/// If there is movement, and you're within the threshold, face legs
+	/// towards the movement.
+	///
+	/// If there is movement but outside of the threshold, apply 'backwards'
+	/// movement logic.
+	/// </summary>
+	/// <returns>The correct rotation based on your current move direction + facing direction.</returns>
+	private Quaternion ChooseLegDirection(Vector2 moveDirection){
+		Quaternion newRotation = Quaternion.identity;
+		
+		// TODO: this is set in the TDC_FaceMouse controller, we should expose this variable
+		FacingDirection _spriteFacingDirection = FacingDirection.UP;
+		Vector2 facingDirection = _faceMouseController.GetFacingDirection();
+		
+		// no movement? Face legs in the direction of facing mouse
+		if (moveDirection.Equals(Vector2.zero)){
+			return GetRotationForStartingDirection(GetAngle(facingDirection), _spriteFacingDirection);
 		}
 		
-		float angle = Mathf.Atan2(directionToFace.y, directionToFace.x) * Mathf.Rad2Deg;
+		Quaternion rotationToFaceMovement = GetRotationForStartingDirection(GetAngle(moveDirection), _spriteFacingDirection);
+		
+		// what's the angle between the facing direction and the moving direction?
+		float angleBetweenThem = Vector2.Angle(moveDirection, facingDirection);
+		
+		// if we're within the normal threshold allowed, then just return the direction of movement
+		// otherwise, we're considered 'moving backwards'
+		if (angleBetweenThem < _turnAngleThreshold){
+			newRotation = rotationToFaceMovement;
+		}else if (angleBetweenThem < 90){
+			Debug.Log("Within 90 part, what to do?");
+		}else{
+			Debug.Log("Angle is past 90 threshold, facing legs inverse of move direction.");
+			newRotation = Quaternion.Inverse(rotationToFaceMovement);
+		}
 
-		// taking initial direction into account, now rotate towards the mouse!
-		transform.rotation = GetRotationForStartingDirection(angle, FacingDirection.UP);
+		return newRotation;
 	}
 
-	protected Quaternion GetRotationForStartingDirection(float angle, FacingDirection direction) {
+	private Quaternion GetRotationForStartingDirection(float angle, FacingDirection direction) {
 		return Quaternion.AngleAxis((angle + (float) direction) % 360, Vector3.forward);
 	}
 
-	/// <summary>
-	/// TODO: implement me! Will play the backwards walking animation if the player is
-	/// facing left, but walking right.
-	/// </summary>
-	private void WalkBackwardsIfFacingOpposite(){
-		
+	private float GetAngle(Vector2 direction){
+		return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 	}
 }
