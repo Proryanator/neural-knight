18fe4ac7feb3b5d1f028ec1dc95af9647ffca7ef
Ryan Laseter <proryanator@gmail.com>
2020-07-18 20:09:49 -0500

    Adding in player boundary triggers, new layer for player boundary! It works! :D

---
+++ Assets/Scripts/Maps/PlayerBoundaries/PlayerBoundary.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using UnityEngine;
+
+public class PlayerBoundary : MonoBehaviour{
+
+	private Collider2D _collider2D;
+	
+	private void Awake(){
+		_collider2D = gameObject.GetComponent<Collider2D>();
+		
+		// when awoken, initially disable the collider
+		DisableCollider();
+	}
+
+	public void DisableCollider(){
+		_collider2D.enabled = false;
+	}
+
+	public void EnableCollider(){
+		_collider2D.enabled = true;
+	}
+}
