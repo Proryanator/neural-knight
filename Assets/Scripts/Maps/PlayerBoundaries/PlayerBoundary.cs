6893e2f7140c3c424a9af157287f935fd1996ed3
Ryan Laseter <proryanator@gmail.com>
2020-08-04 21:13:27 -0500

    Adding in electricity animation! :D movin prefabs around

--- Assets/Scripts/Maps/PlayerBoundaries/PlayerBoundary.cs
+++ Assets/Scripts/Maps/PlayerBoundaries/PlayerBoundary.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerBoundary : MonoBehaviour{
 
 	private Collider2D _collider2D;
+
+	private Transform _childSpriteAnimation;
 	
 	private void Awake(){
 		_collider2D = gameObject.GetComponent<Collider2D>();
+
+		_childSpriteAnimation = transform.GetChild(0);
 		
 		// when awoken, initially disable the collider
 		DisableCollider();
@@ -15,9 +19,13 @@ public class PlayerBoundary : MonoBehaviour{
 
 	public void DisableCollider(){
 		_collider2D.enabled = false;
+		
+		// disable all children
+		_childSpriteAnimation.gameObject.SetActive(false);
 	}
 
 	public void EnableCollider(){
 		_collider2D.enabled = true;
+		_childSpriteAnimation.gameObject.SetActive(true);
 	}
 }
