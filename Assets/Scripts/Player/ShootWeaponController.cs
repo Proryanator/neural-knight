19a5b21ed1f334acc6427e225b830d88d4e22dba
Ryan Laseter <proryanator@gmail.com>
2020-08-15 22:30:37 -0500

    Adding player namespace :D

--- Assets/Scripts/Player/ShootWeaponController.cs
+++ Assets/Scripts/Player/ShootWeaponController.cs
@@ -3,24 +3,26 @@ using Proryanator.Controllers2D;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/// <summary>
-/// Controls when the player takes input, and performs an attack action.
-/// </summary>
-[RequireComponent(typeof(WeaponManager))]
-public class ShootWeaponController : MonoBehaviour{
-
-	private WeaponManager _weaponManager;
-	private TDC_FaceMouse _faceMouseController;
-
-	private void Start(){
-		_weaponManager = GetComponent<WeaponManager>();
-		_faceMouseController = GetComponent<TDC_FaceMouse>();
-	}
-
+namespace Player{
 	/// <summary>
-	/// Fires the currently equipped weapon. Accesses the WeaponManager class to gain this information.
+	/// Controls when the player takes input, and performs an attack action.
 	/// </summary>
-	public void FireWeapon(InputAction.CallbackContext context){
-		_weaponManager.GetEquippedWeapon().Fire(transform, _faceMouseController.GetFacingDirection());
+	[RequireComponent(typeof(WeaponManager))]
+	public class ShootWeaponController : MonoBehaviour{
+
+		private WeaponManager _weaponManager;
+		private TDC_FaceMouse _faceMouseController;
+
+		private void Start(){
+			_weaponManager = GetComponent<WeaponManager>();
+			_faceMouseController = GetComponent<TDC_FaceMouse>();
+		}
+
+		/// <summary>
+		/// Fires the currently equipped weapon. Accesses the WeaponManager class to gain this information.
+		/// </summary>
+		public void FireWeapon(InputAction.CallbackContext context){
+			_weaponManager.GetEquippedWeapon().Fire(transform, _faceMouseController.GetFacingDirection());
+		}
 	}
 }
