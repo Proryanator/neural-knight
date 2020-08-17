28a58f266d63b414a6c04fe1a019ea0b6a7016d9
Ryan Laseter <proryanator@gmail.com>
2020-08-16 19:21:25 -0500

    Cleanup of data point object.

--- Assets/Scripts/DataPoints/DataPoint.cs
+++ Assets/Scripts/DataPoints/DataPoint.cs
@@ -2,19 +2,15 @@
 using UnityEngine;
 
 namespace DataPoints{ 
-	/**
-	* Holds information about this data point.
-	*
-	* Will hold things like how many points this is worth, what type of data point it is, etc.
-	*/
+	/// <summary>
+	/// Represents a collect-able data point in-game.
+	/// </summary>
 	public class DataPoint : MonoBehaviour{
-		[Tooltip("The point value that is acquired/lost when this object is 'collected'.")] [SerializeField]
-		private int _collectionValue;
+		[Tooltip("The value of this data point.")]
+		[SerializeField] private int _collectionValue;
 
 		/// <summary>
 		/// Called when this object is collected.
-		///
-		/// Handles the differences (if any) when collected.
 		/// </summary>
 		public void Collect(PlayerPoints playerPoints){
 			playerPoints.AddPoints(_collectionValue);
