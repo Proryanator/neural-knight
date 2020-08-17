770485f84de241dbec82ebc04b850151b6c236b7
Ryan Laseter <proryanator@gmail.com>
2020-08-17 17:13:24 -0500

    Borrowing best parts!

--- Assets/Scripts/BuildProject.cs
+++ Assets/Scripts/BuildProject.cs
@@ -8,17 +8,14 @@ public class BuildProject{
 	private const string SCENE_PREFIX = "Assets/Scenes/";
 	private static string[] sceneNames = { "first-map" };
 
-	private const string TARGET_DIR =
-		"C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins.jenkins/workspace/Neural Defender/artifacts/";
-	
-	private const string APP_NAME = "NeuralDefender";
-	
 	static void PerformBuild (){
+		var args = FindArgs();
+		
 		// let's create some build options!
 		BuildPlayerOptions options = new BuildPlayerOptions();
 		options.scenes = GetScenePaths(SCENE_PREFIX, sceneNames);
 		
-		string fullPathAndName = AdjustToSystemPath(TARGET_DIR + APP_NAME);	
+		string fullPathAndName = AdjustToSystemPath(args.targetDir + args.appName);	
 
 		System.Console.WriteLine("Output directory will be: [" + fullPathAndName + "]");
 		
@@ -53,5 +50,50 @@ public class BuildProject{
 	/// </summary>
 	private static string AdjustToSystemPath(string filepath){
 		return filepath.Replace(Path.DirectorySeparatorChar, '/');
+	}
+	
+	private static Args FindArgs()
+	{
+		var returnValue = new Args();
+ 
+		// find: -executeMethod
+		//   +1: JenkinsBuild.BuildMacOS
+		//   +2: FindTheGnome
+		//   +3: D:\Jenkins\Builds\Find the Gnome\47\output
+		string[] args = System.Environment.GetCommandLineArgs();
+		var execMethodArgPos = -1;
+		bool allArgsFound = false;
+		for (int i = 0; i < args.Length; i++)
+		{
+			if (args[i] == "-executeMethod")
+			{
+				execMethodArgPos = i;
+			}
+			var realPos = execMethodArgPos == -1 ? -1 : i - execMethodArgPos - 2;
+			if (realPos < 0)
+				continue;
+ 
+			if (realPos == 0)
+				returnValue.appName = args[i];
+			if (realPos == 1)
+			{
+				returnValue.targetDir = args[i];
+				if (!returnValue.targetDir.EndsWith(System.IO.Path.DirectorySeparatorChar + ""))
+					returnValue.targetDir += System.IO.Path.DirectorySeparatorChar;
+ 
+				allArgsFound = true;
+			}
+		}
+ 
+		if (!allArgsFound)
+			System.Console.WriteLine("[JenkinsBuild] Incorrect Parameters for -executeMethod Format: -executeMethod JenkinsBuild.BuildWindows64 <app name> <output dir>");
+ 
+		return returnValue;
+	}
+	
+	private class Args
+	{
+		public string appName = "AppName";
+		public string targetDir = "~/Desktop";
 	}
 }
