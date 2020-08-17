a3c39f5dfca9461305d6867e2a6c6fea470a4b5c
Ryan Laseter <proryanator@gmail.com>
2020-08-17 14:00:50 -0500

    Adding full path for saving build, w/ file name

--- Assets/Scripts/BuildProject.cs
+++ Assets/Scripts/BuildProject.cs
@@ -7,18 +7,23 @@ public class BuildProject{
 	// define the scenes to build for this project
 	private const string SCENE_PREFIX = "Assets/Scenes/";
 	private static string[] sceneNames = { "first-map" };
+
+	private const string TARGET_DIR = "~/Desktop";
+	private const string APP_NAME = "NeuralDefender";
 	
 	static void PerformBuild (){
 		// let's create some build options!
 		BuildPlayerOptions options = new BuildPlayerOptions();
 		options.scenes = GetScenePaths(SCENE_PREFIX, sceneNames);
-
+		
+		string fullPathAndName = TARGET_DIR + Path.DirectorySeparatorChar + APP_NAME + ".exe";
+		
 		// note: can we make this dynamic or build it to multiple platforms?
 		options.target = BuildTarget.StandaloneWindows64;
 		options.targetGroup = BuildTargetGroup.Standalone;
 		
 		// will this create that folder if it doesn't exist?
-		options.locationPathName = "Builds";
+		options.locationPathName = fullPathAndName;
 
 		BuildPipeline.BuildPlayer(options);
 	}
