ad687613d6f0527ccb1f0dd4d90ce9fc95cacb5c
Ryan Laseter <proryanator@gmail.com>
2020-09-07 21:53:28 -0500

    Some more code cleanup, and deleting unused stuff :D

--- Assets/Scripts/Entities/MovementPatterns/AbstractMovementPattern.cs
+++ Assets/Scripts/Entities/MovementPatterns/AbstractMovementPattern.cs
@@ -1,14 +1,9 @@
 ﻿using UnityEngine;
 
 namespace Entities.MovementPatterns{
-	/// <summary>
-	/// A class that holds information that you'd need about how an object
-	/// moves around in 2D space.
-	/// </summary>
 	public abstract class AbstractMovementPattern : MonoBehaviour{
-
-		[Tooltip("The starting facing direction of the sprite")] 
-		[SerializeField] protected FacingDirection startingDirection = FacingDirection.UP;
+		
+		protected FacingDirection startingDirection = FacingDirection.UP;
 		
 		/// <summary>
 		/// Defines the movement pattern for this object.
--- Assets/Scripts/Entities/MovementPatterns/CircleAroundPointPattern.cs
+++ Assets/Scripts/Entities/MovementPatterns/CircleAroundPointPattern.cs
@@ -3,11 +3,6 @@ using Utils;
 
 namespace Entities.MovementPatterns{
 	
-	/// <summary>
-	/// Moves the object around a point, in a circle radius.
-	///
-	/// If set properly can just move to the center of the map instead.
-	/// </summary>
 	public class CircleAroundPointPattern : AbstractMovementPattern{
 		
 		[Tooltip("How far from the point to rotate outwards from. NOTE: this is used to create a slightly larger radius for movement purposes.")]
@@ -42,6 +37,40 @@ namespace Entities.MovementPatterns{
 
 		public static void CircleAroundCenterPointPattern(Transform transform, Transform rotationPoint, float radius, float moveSpeed){
 			// use the circle collider if there is one, or add one if not
+			CircleCollider2D circleCollider2D = AddCircleColliderIfNone(rotationPoint, radius);
+
+			Vector2 directionOfMovement;
+			
+			if (IsInsideOfCircle(transform, circleCollider2D)){
+				directionOfMovement = MoveInCircle(transform, rotationPoint, moveSpeed);
+			}
+			else{
+				directionOfMovement = MoveToCircle(transform, rotationPoint, moveSpeed);
+			}
+
+			// no matter what, you'll want to face your movement direction
+			transform.rotation = Utils2D.GetRotationTowardsDirection(directionOfMovement, FacingDirection.UP);
+		}
+
+		private static Vector2 MoveToCircle(Transform transform, Transform rotationPoint, float moveSpeed){
+			// start moving towards the center, like you do with the MoveToCenterPattern
+			MoveToCenterMovementPattern.MoveToCenterMovement(transform, rotationPoint, moveSpeed);
+				
+			// move towards the center until you're inside of the circle
+			return (Vector2) rotationPoint.position - (Vector2) transform.position;
+		}
+
+		private static Vector2 MoveInCircle(Transform transform, Transform rotationPoint, float moveSpeed){
+			// remember transform from before
+			Vector2 oldPosition = transform.position;
+			
+			transform.RotateAround(rotationPoint.position, Vector3.back, Time.deltaTime * (moveSpeed * 20));
+			
+			// now, let's calculate the direction from one point to another
+			return (Vector2) transform.position - oldPosition;
+		}
+		
+		private static CircleCollider2D AddCircleColliderIfNone(Transform rotationPoint, float radius){
 			CircleCollider2D circleCollider2D = rotationPoint.GetComponent<CircleCollider2D>();
 			if (circleCollider2D == null){
 				circleCollider2D = rotationPoint.gameObject.AddComponent<CircleCollider2D>();
@@ -49,36 +78,12 @@ namespace Entities.MovementPatterns{
 			
 			// make sure this is a trigger, otherwise it'll collide :o
 			circleCollider2D.isTrigger = true;
-			
 			circleCollider2D.radius = radius;
-			
-			Vector2 directionOfMovement;
-			
-			if (IsInsideOfCircle(transform, circleCollider2D)){
-				// remember transform from before
-				Vector2 oldPosition = transform.position;
-			
-				transform.RotateAround(rotationPoint.position, Vector3.back, Time.deltaTime * (moveSpeed * 20));
-			
-				// now, let's calculate the direction from one point to another
-				directionOfMovement = (Vector2) transform.position - oldPosition;
-			}
-			else{
-				// start moving towards the center, like you do with the MoveToCenterPattern
-				MoveToCenterMovementPattern.MoveToCenterMovement(transform, rotationPoint, moveSpeed);
-				
-				// move towards the center until you're inside of the circle
-				directionOfMovement = (Vector2) rotationPoint.position - (Vector2) transform.position;
-			}
 
-			// no matter what, you'll want to face your movement direction
-			transform.rotation = Utils2D.GetRotationTowardsDirection(directionOfMovement, FacingDirection.UP);
+			return circleCollider2D;
 		}
-
-		/// <summary>
-		/// Determine if the enemy is inside of the circle to begin the movement!
-		/// </summary>
-		private static bool IsInsideOfCircle(Transform transform, CircleCollider2D circleCollider2D){
+		
+		private static bool IsInsideOfCircle(Transform transform, Collider2D circleCollider2D){
 			return circleCollider2D.bounds.Contains(transform.position);
 		}
 	}
