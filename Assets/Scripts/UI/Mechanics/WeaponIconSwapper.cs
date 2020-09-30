b893ab1477f94217d2b4ed3a71079aabea4714cf
Ryan Laseter <proryanator@gmail.com>
2020-09-29 20:26:14 -0500

    Note about future re-factor

--- Assets/Scripts/UI/Mechanics/WeaponIconSwapper.cs
+++ Assets/Scripts/UI/Mechanics/WeaponIconSwapper.cs
@@ -12,6 +12,8 @@ namespace UI.Mechanics{
 
 		private void Start(){
 			// NOTE: this is fragile, look for a better way to access this
+			// pretty sure there's some design pattern where we could hook many classes together,
+			// and prevent them from even knowing about each other!
 			WeaponManager weaponManager = FindObjectOfType<WeaponManager>();
 			weaponManager.OnWeaponChange += UpdateWeaponImage;
 		}
