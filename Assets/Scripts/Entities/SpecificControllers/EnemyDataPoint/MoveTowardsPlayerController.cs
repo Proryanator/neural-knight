38bbe955d26343e3b04c3c2be7b026a31850d1fd
Ryan Laseter <proryanator@gmail.com>
2021-01-24 15:17:07 -0600

    Adding logic for the enemy data point to move towards the player at a given speed when the player gets close enough (these behave differently than the larger data points).

---
+++ Assets/Scripts/Entities/SpecificControllers/EnemyDataPoint/MoveTowardsPlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Utils;
+
+namespace Entities.SpecificControllers.EnemyDataPoint {
+	public class MoveTowardsPlayerController : MonoBehaviour {
+
+		private Transform _playerTransform;
+		private bool _enabled;
+
+		private float _lerpAmount;
+		[SerializeField] private float _lerpFactor = 0.1f;
+
+		private void Start(){
+			_playerTransform = GameObject.FindGameObjectWithTag(AllTags.PLAYER).transform;
+		}
+
+		private void Update(){
+			if (_enabled){
+				_lerpAmount += Time.deltaTime;
+				transform.position = Vector2.Lerp(transform.position, _playerTransform.position, _lerpAmount * _lerpFactor);
+			}
+			else{
+				_lerpAmount = 0f;
+			}
+		}
+
+		private void OnTriggerEnter2D(Collider2D other){
+			if (other.gameObject.tag.Equals(AllTags.PLAYER)){
+				_enabled = true;
+			}
+		}
+	}
+}
