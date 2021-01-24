6a213807660b980602a52295701d534e1dda2a76
Ryan Laseter <proryanator@gmail.com>
2021-01-24 15:52:22 -0600

    Noice, fixing bug where points/ammo boxes are leftover after the scene/game ends

--- Assets/Scripts/Entities/Events/DeSpawnable.cs
+++ Assets/Scripts/Entities/Events/DeSpawnable.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 namespace Entities.Events{
 	public class DeSpawnable : MonoBehaviour{
 
+		private bool _isQuitting;
+		
 		// an event meant to attach any void method to when this object is despawned
 		public Action OnDeSpawn;
-		
+
+		private void OnApplicationQuit(){
+			_isQuitting = true;
+		}
+
 		private void OnDestroy(){
-			OnDeSpawn?.Invoke();
+			// only call this if we're not exiting the application (this could cause some weird side-effects
+			if (!_isQuitting){
+				OnDeSpawn?.Invoke();
+			}
 		}
 	}
 }
