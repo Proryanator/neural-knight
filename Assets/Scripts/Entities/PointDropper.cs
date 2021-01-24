5ebf6be376d18832e9eaa8cf7c4602f9d31520ff
Ryan Laseter <proryanator@gmail.com>
2021-01-24 14:08:19 -0600

    Added point dropper to enemy prefab, also adding in speed away from the player to move the points around

--- Assets/Scripts/Entities/PointDropper.cs
+++ Assets/Scripts/Entities/PointDropper.cs
@@ -8,6 +8,12 @@ namespace Entities {
 
 		// might change how many of these point values are dropped
 		private int _totalToSpawn;
+
+		// the angle modification range possible for random movement
+		// a data point can move away from the player at any angle +- this modifier
+		[SerializeField] private float _angleModifier = 30f;
+
+		[SerializeField] private float _speed = 3f;
 		
 		private void Awake(){
 			// attach the spawn method to the despawn method of it's parent
@@ -20,8 +26,22 @@ namespace Entities {
 			for (int i = 0; i < _totalToSpawn; i++){
 				GameObject newEntityDataPoint = Instantiate(_pointPrefab, transform.position, Quaternion.identity);
 				// parent to the room object so that it disappears when player's leave the map
-				newEntityDataPoint.transform.SetParent(GameObject.FindGameObjectWithTag(AllTags.ROOM).transform);	
+				newEntityDataPoint.transform.SetParent(GameObject.FindGameObjectWithTag(AllTags.ROOM).transform);
+				
+				// set an initial direction away from the player to move the point objects
+				Vector2 playerPosition = GameObject.FindGameObjectWithTag(AllTags.PLAYER).transform.position;
+				newEntityDataPoint.GetComponent<Rigidbody2D>().velocity
+					= RandomDirectionFromPlayer(newEntityDataPoint.transform.position,
+						playerPosition,
+						_speed);
 			}
+		}
+
+		private Vector2 RandomDirectionFromPlayer(Vector2 pointStartPosition, Vector2 playerPosition, float speed){
+			// calculate normalized direction away from the player
+			Vector2 directionOfMovement = (pointStartPosition - playerPosition).normalized;
+
+			return directionOfMovement * speed;
 		}
 	}
 }
