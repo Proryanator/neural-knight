e3bfc1bb76d30a943766d5b810fa6e5f7312d8af
Ryan Laseter <proryanator@gmail.com>
2021-01-24 13:54:06 -0600

    Adding in point spawner for the entity

---
+++ Assets/Scripts/Entities/PointDropper.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Events;
+using UnityEngine;
+using Utils;
+
+namespace Entities {
+	public class PointDropper : MonoBehaviour {
+		[SerializeField] private GameObject _pointPrefab;
+
+		// might change how many of these point values are dropped
+		private int _totalToSpawn;
+		
+		private void Awake(){
+			// attach the spawn method to the despawn method of it's parent
+			GetComponent<DeSpawnable>().OnDeSpawn += SpawnPoints;
+			
+			_totalToSpawn = Random.Range(1, 5);
+		}
+
+		private void SpawnPoints(){
+			for (int i = 0; i < _totalToSpawn; i++){
+				GameObject newEntityDataPoint = Instantiate(_pointPrefab, transform.position, Quaternion.identity);
+				// parent to the room object so that it disappears when player's leave the map
+				newEntityDataPoint.transform.SetParent(GameObject.FindGameObjectWithTag(AllTags.ROOM).transform);	
+			}
+		}
+	}
+}
