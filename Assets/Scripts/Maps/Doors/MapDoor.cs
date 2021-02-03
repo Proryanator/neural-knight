0d8285d04ece69a3872b4323d55d782cf2a38400
Ryan Laseter <proryanator@gmail.com>
2021-02-02 19:40:06 -0600

    Adding door textures into the scene, making prefab, in preparation for map navigation mechanics

---
+++ Assets/Scripts/Maps/Doors/MapDoor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Maps {
+	public class MapDoor : MonoBehaviour {
+
+		private SpriteRenderer _spriteRenderer;
+		private Collider2D _collider2D;
+
+		private void Awake(){
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+			_collider2D = GetComponent<Collider2D>();
+		}
+
+		public void EnableDoor(){
+			_collider2D.enabled = true;
+			_spriteRenderer.enabled = true;
+		}
+
+		public void DisableDoor(){
+			_collider2D.enabled = false;
+			_spriteRenderer.enabled = false;
+		}
+	}
+}
